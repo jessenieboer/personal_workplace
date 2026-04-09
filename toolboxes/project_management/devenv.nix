323c04020dfb9ebcc1e553039059da4456086669
Jesse Nieboer <jessenieboer@protonmail.com>
2026-04-09 13:10:53 -0500

    Add existing project management stuff

--- toolboxes/project_management/devenv.nix
+++ toolboxes/project_management/devenv.nix
@@ -1,12 +1,18 @@
 { pkgs, lib, config, inputs, ... }:
 
 {
-  packages = [
-    pkgs.hello
-  ];
-
-  # https://devenv.sh/basics/
   enterShell = ''
     echo "jessenieboer's project management toolbox available"
   '';
+
+  files = {
+    ".envrc".text = ''
+      #!/usr/bin/env bash
+
+      eval "$(devenv direnvrc)"
+    '';
+
+    "project_management/project_info.org".source = ./templates/project_info.org;
+    "project_management/static_config.org".source = ./configs/static_config.org;
+  };
 }
