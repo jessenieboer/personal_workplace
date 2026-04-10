e84af49046be0143ecfaa89b532a91c4ae948f4a
Jesse Nieboer <jessenieboer@protonmail.com>
2026-04-10 10:42:03 -0500

    Add proof-of-concept project config

--- toolboxes/project_management/devenv.nix
+++ toolboxes/project_management/devenv.nix
@@ -1,18 +1,47 @@
 { pkgs, lib, config, inputs, ... }:
 
 {
-  enterShell = ''
-    echo "jessenieboer's project management toolbox available"
-  '';
+  # files = {
+    # ".envrc".text = ''
+    #   #!/usr/bin/env bash
 
-  files = {
-    ".envrc".text = ''
-      #!/usr/bin/env bash
+    #   eval "$(devenv direnvrc)"
+    # '';
+    # };
 
-      eval "$(devenv direnvrc)"
-    '';
+    config = {
+      enterShell = ''
+        echo "jessenieboer's project management toolbox available"
+      '';
 
-    "project_management/project_info.org".source = ./templates/project_info.org;
-    "project_management/static_config.org".source = ./configs/static_config.org;
-  };
+      project_management = {
+        project_name = "my-super-app";
+      };
+
+      # todo use pkgs.replaceVars instead of sed?
+      tasks = {
+        "project_management_toolbox:setup" = {
+          before = [ "devenv:enterShell" ];
+          exec = ''
+            echo "Generating .dir-locals.el (project_name: ${config.project_management.project_name})"
+
+            sed "s|@PROJECT_NAME@|${config.project_management.project_name}|g" \
+            templates/dir-locals.el.in > .dir-locals.el
+
+            echo ".dir-locals.el generated successfully"
+          '';
+          showOutput = true;
+        };
+      };
+    };
+
+    options = {
+      project_management = {
+        project_name = lib.mkOption {
+          description = "The name of this project";
+          example = "My cool project";
+          type = lib.types.str;
+        };
+      };
+    };
 }
