d6b3bdda4dad41b6def47762f6e3555c5bcbd172
Jesse Nieboer <jessenieboer@protonmail.com>
2026-04-14 11:08:27 -0500

    Add git toolbox

--- toolboxes/project_management/devenv.nix
+++ toolboxes/project_management/devenv.nix
@@ -40,19 +40,6 @@ in
         ];        
         showOutput = true;
       };
-      # "project_management_toolbox:update_gitignore" = {
-      #   before = [ "devenv:enterShell" ];
-      #   exec = ''
-      #     if [ ! -f .gitignore ]; then
-      #     rsync -a --chmod=a+rw ${gitIgnore} .gitignore
-      #     echo "Copied new .gitignore"
-      #     else
-      #     cat ${gitIgnore} .gitignore | sort -u >  .gitignore.tmp && mv .gitignore.tmp .gitignore
-      #     echo "Updated existing .gitignore"
-      #     fi
-      #   '';      
-      #   showOutput = true;
-      # };
     };
   };
 
