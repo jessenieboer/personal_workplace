7607753aa780301105416fd1ad121de0f06ccbfe
Jesse Nieboer <jessenieboer@protonmail.com>
2026-04-14 10:56:35 -0500

    Add minor tweaks

--- toolboxes/project_management/devenv.nix
+++ toolboxes/project_management/devenv.nix
@@ -1,7 +1,7 @@
 { pkgs, lib, config, inputs, ... }:
 let
   dirLocalsTemplate = ./templates/dir-locals.el.in;
-  gitIgnore = ./configs/.gitignore;
+  #gitIgnore = ./configs/.gitignore;
 in
 {
   config = {
@@ -26,11 +26,11 @@ in
       "project_management_toolbox:generate_dir_locals" = {
         before = [ "devenv:enterShell" ];
         exec = let
-          subprojectDirs = lib.concatStringsSep " " config.project_management.subproject_directories;
+          subprojectManagementDirs = lib.concatStringsSep " " config.project_management.subproject_management_directories;
         in ''
           sed -e "s|@PROJECT_NAME@|${config.project_management.project_name}|g" \
-          -e "s|@PROJECT_DIRECTORY@|${config.devenv.root}|g" \
-          -e "s|@SUBPROJECT_DIRECTORIES@|${subprojectDirs}|g" \
+          -e "s|@PROJECT_MANAGEMENT_DIRECTORY@|${config.devenv.root}|g" \
+          -e "s|@SUBPROJECT_MANAGEMENT_DIRECTORIES@|${subprojectManagementDirs}|g" \
           ${dirLocalsTemplate} > .dir-locals.el
 
           echo ".dir-locals.el generated successfully"
@@ -40,19 +40,19 @@ in
         ];        
         showOutput = true;
       };
-      "project_management_toolbox:update_gitignore" = {
-        before = [ "devenv:enterShell" ];
-        exec = ''
-          if [ ! -f .gitignore ]; then
-          rsync -a --chmod=a+rw ${gitIgnore} .gitignore
-          echo "Copied new .gitignore"
-          else
-          cat ${gitIgnore} .gitignore | sort -u >  .gitignore.tmp && mv .gitignore.tmp .gitignore
-          echo "Updated existing .gitignore"
-          fi
-        '';      
-        showOutput = true;
-      };
+      # "project_management_toolbox:update_gitignore" = {
+      #   before = [ "devenv:enterShell" ];
+      #   exec = ''
+      #     if [ ! -f .gitignore ]; then
+      #     rsync -a --chmod=a+rw ${gitIgnore} .gitignore
+      #     echo "Copied new .gitignore"
+      #     else
+      #     cat ${gitIgnore} .gitignore | sort -u >  .gitignore.tmp && mv .gitignore.tmp .gitignore
+      #     echo "Updated existing .gitignore"
+      #     fi
+      #   '';      
+      #   showOutput = true;
+      # };
     };
   };
 
@@ -64,10 +64,10 @@ in
         type = lib.types.str;
       };
 
-      subproject_directories = lib.mkOption {
+      subproject_management_directories = lib.mkOption {
         type = lib.types.listOf lib.types.str;
         default = [ ];
-        description = "List of directories to subprojects";
+        description = "List of project management directories of subprojects";
         example = [ "/path/to/subproj1/" "/path/to/subproj2/"];
       };
     };
