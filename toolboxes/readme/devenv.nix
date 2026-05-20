6ddbc537081f9dfba9e6af6ab60426504bb8c149
Jesse Nieboer <jessenieboer@protonmail.com>
2026-05-20 10:47:19 -0500

    Refactor readme stuff

--- toolboxes/readme/devenv.nix
+++ toolboxes/readme/devenv.nix
@@ -12,15 +12,15 @@ in
       "readme_toolbox:copy_readme_template" = {
         before = [ "devenv:enterShell" ];
         exec = ''
-          if [ -f "${config.devenv.root}/docs/readme.org" ]; then
-          echo "docs readme.org already exists — skipping copy."
+          if [ -f "${config.devenv.root}/toolboxes/readme_toolbox/readme.org" ]; then
+          echo "readme.org already exists — skipping copy."
           exit 0
           fi
-          mkdir -p ${config.devenv.root}/docs
-          cp ${readmeTemplate} ${config.devenv.root}/docs/readme.org
-          chmod u+w ${config.devenv.root}/docs/readme.org
+          mkdir -p ${config.devenv.root}/toolboxes/readme_toolbox
+          cp ${readmeTemplate} ${config.devenv.root}/toolboxes/readme_toolbox/readme.org
+          chmod u+w ${config.devenv.root}/toolboxes/readme_toolbox/readme.org
 
-          echo "copied readme template to docs/readme.org"
+          echo "copied readme template to toolboxes/readme_toolbox/readme.org"
         '';
         showOutput = true;
       };
