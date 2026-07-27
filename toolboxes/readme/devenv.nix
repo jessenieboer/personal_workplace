be2dc6104251c8dd7c05a4e42e3e295c8592ea12
Jesse Nieboer <jessenieboer@protonmail.com>
2026-07-27 11:58:35 -0500

    Update readme toolbox to use .toolboxes

--- toolboxes/readme/devenv.nix
+++ toolboxes/readme/devenv.nix
@@ -1,3 +1,4 @@
+
 { pkgs, lib, config, inputs, ... }:
 let
   readmeTemplate = ./templates/readme.org;
@@ -12,15 +13,15 @@ in
       "readme_toolbox:copy_readme_template" = {
         before = [ "devenv:enterShell" ];
         exec = ''
-          if [ -f "${config.devenv.root}/toolboxes/readme_toolbox/readme.org" ]; then
+          if [ -f "${config.devenv.root}/.toolboxes/readme_toolbox/readme.org" ]; then
           echo "readme.org already exists — skipping copy."
           exit 0
           fi
-          mkdir -p ${config.devenv.root}/toolboxes/readme_toolbox
-          cp ${readmeTemplate} ${config.devenv.root}/toolboxes/readme_toolbox/readme.org
-          chmod u+w ${config.devenv.root}/toolboxes/readme_toolbox/readme.org
+          mkdir -p ${config.devenv.root}/.toolboxes/readme_toolbox
+          cp ${readmeTemplate} ${config.devenv.root}/.toolboxes/readme_toolbox/readme.org
+          chmod u+w ${config.devenv.root}/.toolboxes/readme_toolbox/readme.org
 
-          echo "copied readme template to toolboxes/readme_toolbox/readme.org"
+          echo "copied readme template to .toolboxes/readme_toolbox/readme.org"
         '';
         showOutput = true;
       };
