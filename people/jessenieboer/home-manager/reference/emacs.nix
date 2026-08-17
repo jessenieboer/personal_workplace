b4317d289efda4831d6fa61d7d3dfebac5f2bbeb
Jesse Nieboer <jessenieboer@protonmail.com>
2026-08-17 18:01:52 -0500

    Use devenv and toolboxes at top level

--- people/jessenieboer/home-manager/reference/emacs.nix
+++ people/jessenieboer/home-manager/reference/emacs.nix
@@ -1,3 +1,4 @@
+# old file for reference only
 {
   config,
   lib,
--- toolboxes/git/devenv.nix
+++ toolboxes/git/devenv.nix
@@ -10,7 +10,7 @@ in
       {
       cat ${gitIgnore}
       echo
-      find . -name .gitignore -type f -exec cat {} \; -exec echo \;
+      find . -path '*/.toolboxes/*' -name .gitignore -type f -exec cat {} \; -exec echo \;
       } 2>/dev/null | grep -v '^$' | sort -u > "$tmp" && mv "$tmp" .gitignore
       echo "Merged template + all nested .gitignore files into ./.gitignore"
       echo "jessenieboer's git toolbox available"
